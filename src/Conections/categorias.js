5397896b09589c7839269a3bc82de6a9db344e8f
Cristian Gael Valenzuela Castro <elgael06@gmail.com>
2020-10-23 13:42:15 -0600

    Update categorias.js

--- src/Conections/categorias.js
+++ src/Conections/categorias.js
@@ -1,7 +1,7 @@
 import Axios from 'axios';
 
 const ObtenerCategoorias = (set, err) => {
-    Axios.get("http://creditienda.mx/api/categorias")
+    Axios.get("https://creditienda.mx/api/categorias")
         .then(res => {
             res.status === 200 ? set(res.data) : err()
         })

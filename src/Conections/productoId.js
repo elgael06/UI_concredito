4ca67dddc533ec9efe880abcf20c221252e5b27b
Cristian Gael Valenzuela Castro <elgael06@gmail.com>
2020-10-23 13:41:52 -0600

    Update productoId.js

--- src/Conections/productoId.js
+++ src/Conections/productoId.js
@@ -1,7 +1,7 @@
 import Axios from 'axios';
 
 const ObtenerProductoID = async id =>{
-    let respuesta = await Axios.get(`http://creditienda.mx/api/productos/elastic/${id}`)
+    let respuesta = await Axios.get(`https://creditienda.mx/api/productos/elastic/${id}`)
     return respuesta.status===200 ? respuesta.data : null;
 }
 
